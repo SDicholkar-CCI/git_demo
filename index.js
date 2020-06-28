16d63ec5d7e164a415ed98e4029f94dfd93f7ad7
SDicholkar-CCI <swapnil.dicholkar@creativecapsule.com>
2020-06-28 11:24:55 +0530

    PRO-01 : completed changes for PRO-01

--- index.js
+++ index.js
@@ -12,13 +12,19 @@ var corsOptions = {
 }
 app.use(cors(corsOptions))
 
+app.listen(3000, () => console.log(`started port PRO-01`)); //PRO-01
+
+console.log('PRO-01 : added backend changes');//PRO-01 : backend changes
 mongoose.connect('mongodb://root:rootpassword@localhost:29020/scratchCard?authSource=admin', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDb...'))
     .catch(error => console.log('Could not connect to MongoDb...', error));
 
+console.log('PRO-01 : added backend changes');//PRO-01 : backend changes
 app.use(express.json());
 app.use('/user', user);
 app.use('/scratchCard', scratchCard);
 app.use('/transaction', transaction);
 
 app.listen(3000, () => console.log(`started port`));
+
+app.listen(3000, () => console.log(`started port PRO-01`)); //PRO-01
